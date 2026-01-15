251dc3f9c8b08c1c880798227d6a47dd8d1acd71
jasonmcgetty <jasontmcgetty@yahoo.com>
2026-01-15 11:36:10 -0500

    Refactor project structure and set up frontend react framework (currently has errors)

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+console.log('App started!');
+
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+    res.send('Hello World from Express!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
